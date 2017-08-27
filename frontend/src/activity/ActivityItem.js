7aeca6aba968500f6c933924ef5af280431dfbec
Benjamin Herzig <ber.bherzig@gmail.com>
2017-08-27 23:26:13 +0200

    add icons

--- frontend/src/activity/ActivityItem.js
+++ frontend/src/activity/ActivityItem.js
@@ -1,13 +1,28 @@
 import React from 'react'
-import { ListItem, ListItemText } from 'material-ui/List'
+import { ListItem, ListItemIcon, ListItemText } from 'material-ui/List'
 import moment from 'moment'
+import Done from 'material-ui-icons/Done'
+import Update from 'material-ui-icons/Update'
 
 const ActivityItem = (props) => {
   const { name, start, end, isRunning } = props
-  const duration = `${moment(start).format('HH:mm')} - ${end === undefined ? '' : moment(end).format('HH:mm')}`
+  const format = 'HH:mm'
+  const startTime = moment(start).format(format)
+  const endTime = moment(end).format(format)
+  const duration = moment.duration(moment(end).diff(moment(start))).humanize()
+  const period = `${duration} from ${startTime} to ${end === undefined ? '' : endTime}`
   return (
-    <ListItem className={isRunning ? 'running' : ''}>
-      <ListItemText primary={name} secondary={duration}/>
+    <ListItem>
+      {
+        isRunning ?
+          <ListItemIcon>
+            <Update/>
+          </ListItemIcon> :
+          <ListItemIcon>
+            <Done/>
+          </ListItemIcon>
+      }
+      <ListItemText primary={name} secondary={period}/>
     </ListItem>
   )
 }
