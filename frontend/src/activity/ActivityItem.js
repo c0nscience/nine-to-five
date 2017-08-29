5afb046f1ca4ee8d6c6d7872f20ff08e2c26f63a
Benjamin Herzig <ber.bherzig@gmail.com>
2017-08-29 21:34:32 +0200

    add a touch of color

--- frontend/src/activity/ActivityItem.js
+++ frontend/src/activity/ActivityItem.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import { ListItem, ListItemIcon, ListItemText } from 'material-ui/List'
 import moment from 'moment'
+import { withStyles } from 'material-ui/styles'
+import green from 'material-ui/colors/green'
+import amber from 'material-ui/colors/amber'
 import Done from 'material-ui-icons/Done'
 import Update from 'material-ui-icons/Update'
 
+const styles = theme => ({
+  done: {
+    color: green[500]
+  },
+  running: {
+    color: amber[500]
+  }
+})
+
 const ActivityItem = (props) => {
   const timeFormat = 'HH:mm'
-  const { name, start: startUtc, end: endUtc, isRunning } = props
+  const { name, start: startUtc, end: endUtc, isRunning, classes } = props
 
   const localStart = moment.utc(startUtc).local()
   const localEnd = moment.utc(endUtc).local()
 
   const duration = moment.duration(moment(localEnd).diff(moment(localStart))).humanize()
   const period = `${duration} from ${localStart.format(timeFormat)} ${endUtc === undefined ? '' : `to ${localEnd.format(timeFormat)}`}`
-  const icon = isRunning ? <Update/> : <Done/>
+  const icon = isRunning ? <Update className={classes.running}/> : <Done className={classes.done}/>
 
   return (
     <ListItem>
@@ -25,4 +37,4 @@ const ActivityItem = (props) => {
   )
 }
 
-export default ActivityItem
+export default withStyles(styles)(ActivityItem)
