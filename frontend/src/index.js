306b19a51c04128733aa5ea5ea1429628d5f5c0b
Benjamin Herzig <ber.bherzig@gmail.com>
2017-12-05 09:59:24 +0100

    logout if session is expired on app start

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,12 @@ import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import { Provider } from 'react-redux'
 import store, { history } from './store'
+import { logout } from './reducers/auth'
+
+const state = store.getState()
+if (state.auth && !state.auth.isAuthenticated) {
+  store.dispatch(logout())
+}
 
 ReactDOM.render(
   <Provider store={store}>
