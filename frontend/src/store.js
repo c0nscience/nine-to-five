c2a69f398199c010d7deb788daac476b7b92f778
Benjamin Herzig <ber.bherzig@gmail.com>
2017-12-08 01:08:24 +0100

    adding thunk again because its still used by the session handling

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,9 @@
 import { createBrowserHistory } from 'history'
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import { applyMiddleware, compose, combineReducers, createStore } from 'redux'
 import activity from './reducers/activity'
 import auth from './reducers/auth'
 import error from './reducers/error'
+import thunkMiddleware from 'redux-thunk'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
 import { createEpicMiddleware } from 'redux-observable'
 import { rootEpic } from './epics'
@@ -24,6 +25,7 @@ export default createStore(
   composeEnhancers(
     applyMiddleware(
       epicMiddleware,
+      thunkMiddleware,
       routerMiddleware(history)
     )
   )
