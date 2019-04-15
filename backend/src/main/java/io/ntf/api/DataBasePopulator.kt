a01ba752ea113763b2e1fd14c0535620fdb30175
Benjamin Herzig <ber.bherzig@gmail.com>
2019-04-16 00:59:23 +0200

    replace moment in item component with luxon

---
+++ backend/src/main/java/io/ntf/api/DataBasePopulator.kt
@@ -0,0 +1,32 @@
+package io.ntf.api
+
+import io.ntf.api.activity.model.Activity
+import io.ntf.api.activity.model.ActivityRepository
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
+import reactor.core.publisher.Flux
+import java.time.LocalDateTime
+
+//@Component
+class DataBasePopulator(private val activityRepository: ActivityRepository) : CommandLineRunner {
+
+  private val log = LoggerFactory.getLogger(javaClass)
+
+  override fun run(vararg args: String?) {
+    val now = LocalDateTime.now()
+    val activityTemplate = Activity(userId = "auth0|59ac17508f649c3f85124ec1", name = "template", start = now)
+    Flux.just(
+      activityTemplate.copy(name = "activity one", start = now.minusMonths(1).minusHours(1), end = now.minusMonths(1)),
+      activityTemplate.copy(name = "activity two", start = now.minusMonths(1).minusHours(2), end = now.minusMonths(1)),
+      activityTemplate.copy(name = "activity three", start = now.minusMonths(1).plusHours(1), end = now.minusMonths(1).plusHours(2)),
+      activityTemplate.copy(name = "activity four", start = now.minusMonths(1).plusHours(2), end = now.minusMonths(1).plusHours(3)),
+      activityTemplate.copy(name = "activity five", start = now.minusMonths(0).minusHours(1), end = now.minusMonths(0)),
+      activityTemplate.copy(name = "activity seven", start = now.minusMonths(0).plusHours(0), end = now.minusMonths(0).plusHours(1)),
+      activityTemplate.copy(name = "activity eight", start = now.minusMonths(0).plusHours(2), end = now.minusMonths(0).plusHours(3)),
+      activityTemplate.copy(name = "activity nine", start = now.minusMonths(0).plusHours(3), end = now.minusMonths(0).plusHours(4).plusMinutes(10)),
+      activityTemplate.copy(name = "activity ten", start = now.minusMonths(0).plusHours(4), end = now.minusMonths(0).plusHours(5).plusMinutes(25))
+    )
+      .flatMap { activityRepository.save(it) }
+      .subscribe { log.info("id: ${it.id}") }
+  }
+}
--- frontend/src/activity-old/ActivityItem.js
+++ frontend/src/activity-old/ActivityItem.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import moment from 'moment'
 import {withStyles} from '@material-ui/core/styles'
 import Edit from '@material-ui/icons/Edit'
 import {selectActivity} from '../actions'
-import {amber, green} from "@material-ui/core/colors";
-import IconButton from "@material-ui/core/IconButton";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItem from "@material-ui/core/ListItem/ListItem";
+import {amber, green} from '@material-ui/core/colors'
+import IconButton from '@material-ui/core/IconButton'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItem from '@material-ui/core/ListItem/ListItem'
+import {DateTime} from 'luxon'
 
 const styles = theme => ({
   done: {
@@ -23,11 +23,11 @@ const ActivityItem = (props) => {
   const timeFormat = 'HH:mm'
   const {id, name, start: _start, end: _end} = props
 
-  const end = _end && moment.utc(_end).local()
-  const start = moment.utc(_start).local()
-  const endOrNow = end || moment()
-  const duration = moment.duration(endOrNow.diff(start)).humanize()
-  const period = `${duration} from ${start.format(timeFormat)} ${end === undefined ? '' : `to ${end.format(timeFormat)}`}`
+  const end = _end && DateTime.fromISO(_end).toLocal()
+  const start = DateTime.fromISO(_start).toLocal()
+  const endOrNow = end || DateTime.local()
+  const duration = endOrNow.diff(start).toFormat('h \'hrs\'', {floor: false})
+  const period = `${duration} from ${start.toFormat(timeFormat)} ${end === undefined ? '' : `to ${end.toFormat(timeFormat)}`}`
 
   return (
     <ListItem>
