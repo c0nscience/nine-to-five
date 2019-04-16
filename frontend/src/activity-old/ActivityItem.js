96528d80a96da7871e111a79f555dd2a2165e181
Benjamin Herzig <ber.bherzig@gmail.com>
2019-04-16 10:23:17 +0200

    fix moment handling and update dependencies

--- frontend/src/activity-old/ActivityItem.js
+++ frontend/src/activity-old/ActivityItem.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {withStyles} from '@material-ui/core/styles'
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles'
 import Edit from '@material-ui/icons/Edit'
-import {selectActivity} from '../actions'
-import {amber, green} from '@material-ui/core/colors'
+import { selectActivity } from '../actions'
+import { amber, green } from '@material-ui/core/colors'
 import IconButton from '@material-ui/core/IconButton'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import ListItemText from '@material-ui/core/ListItemText'
 import ListItem from '@material-ui/core/ListItem/ListItem'
-import {DateTime} from 'luxon'
+import { DateTime } from 'luxon'
+import moment from 'moment'
 
 const styles = theme => ({
   done: {
@@ -19,12 +20,22 @@ const styles = theme => ({
   }
 })
 
+const handleMoment = date => {
+  if (moment.isMoment(date)) {
+    console.log('isMoment')
+    return date.toISOString()
+  } else {
+    console.log('is not moment')
+    return date
+  }
+}
+
 const ActivityItem = (props) => {
   const timeFormat = 'HH:mm'
   const {id, name, start: _start, end: _end} = props
 
-  const end = _end && DateTime.fromISO(_end).toLocal()
-  const start = DateTime.fromISO(_start).toLocal()
+  const end = _end && DateTime.fromISO(handleMoment(_end)).toLocal()
+  const start = DateTime.fromISO(handleMoment(_start)).toLocal()
   const endOrNow = end || DateTime.local()
   const duration = endOrNow.diff(start).as('hours')
   const period = `${duration.toFixed(1)} hrs from ${start.toFormat(timeFormat)} ${end === undefined ? '' : `to ${end.toFormat(timeFormat)}`}`
