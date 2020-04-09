97e1351dbf78cf5034fb5ae612e8269b705a30c8
Benjamin Herzig <ber.bherzig@pm.me>
2020-04-09 11:31:25 +0200

    replace custom jwt authentication with spring security tools

--- backend/src/main/kotlin/io/ntf/api/infrastructure/SecurityConfiguration.kt
+++ backend/src/main/kotlin/io/ntf/api/infrastructure/SecurityConfiguration.kt
@@ -1,12 +1,13 @@
 package io.ntf.api.infrastructure
 
-import io.ntf.api.infrastructure.jwt.JwtWebSecurityConfigurer
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod.*
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
+import org.springframework.security.oauth2.jwt.*
 import org.springframework.security.web.server.SecurityWebFilterChain
 
 @EnableWebFluxSecurity
@@ -15,31 +16,42 @@ import org.springframework.security.web.server.SecurityWebFilterChain
 class SecurityConfiguration(private val auth0ConfigurationProperties: Auth0ConfigurationProperties) {
 
   @Bean
-  fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+  fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain =
+    http
+      .authorizeExchange()
+      .pathMatchers(OPTIONS).permitAll()
+
+      .pathMatchers(POST, "/activity").hasAuthority("SCOPE_start:activity")
+      .pathMatchers(POST, "/activity/stop").hasAuthority("SCOPE_stop:activity")
+      .pathMatchers(GET, "/activity/running").hasAuthority("SCOPE_read:activities")
+      .pathMatchers(PUT, "/activity/{id}").hasAuthority("SCOPE_update:activity")
+      .pathMatchers(DELETE, "/activity/{id}").hasAuthority("SCOPE_delete:activity")
+      .pathMatchers(DELETE, "/activity/{id}").hasAuthority("SCOPE_delete:activity")
+      .pathMatchers(GET, "/activities").hasAuthority("SCOPE_read:activities")
+      .pathMatchers(GET, "/activities/{from}/{to}").hasAuthority("SCOPE_read:activities")
+      .pathMatchers(HEAD, "/activities").hasAuthority("SCOPE_read:activities")
+      .pathMatchers(GET, "/activities/tags").hasAuthority("SCOPE_read:activities")
+      .pathMatchers(DELETE, "/activities").hasAuthority("SCOPE_delete:activity")
+
+      .pathMatchers(GET, "/statistics/overtime").hasAuthority("SCOPE_read:overtime")
+      .and()
+      .oauth2ResourceServer().jwt().and().and().build()
+
+  @Bean
+  fun jwtDecoder(): ReactiveJwtDecoder {
     val issuer = auth0ConfigurationProperties.issuer
     val audience = auth0ConfigurationProperties.audience
 
-    JwtWebSecurityConfigurer.forRS256(audience, issuer)
-      .configure(http)
-      .csrf().disable()
-      .authorizeExchange()
-      .pathMatchers(POST, "/activity").hasAuthority("start:activity")
-      .pathMatchers(POST, "/activity/stop").hasAuthority("stop:activity")
-      .pathMatchers(GET, "/activity/running").hasAuthority("read:activities")
-      .pathMatchers(PUT, "/activity/{id}").hasAuthority("update:activity")
-      .pathMatchers(DELETE, "/activity/{id}").hasAuthority("delete:activity")
-      .pathMatchers(DELETE, "/activity/{id}").hasAuthority("delete:activity")
-      .pathMatchers(GET, "/activities").hasAuthority("read:activities")
-      .pathMatchers(GET, "/activities/{from}/{to}").hasAuthority("read:activities")
-      .pathMatchers(HEAD, "/activities").hasAuthority("read:activities")
-      .pathMatchers(GET, "/activities/tags").hasAuthority("read:activities")
-      .pathMatchers(DELETE, "/activities").hasAuthority("delete:activity")
 
-      .pathMatchers(GET, "/statistics/overtime").hasAuthority("read:overtime")
+    val jwtDecoder = ReactiveJwtDecoders.fromOidcIssuerLocation(issuer) as NimbusReactiveJwtDecoder
 
-      .pathMatchers(OPTIONS).permitAll()
+    val audienceValidator = AudienceValidator(audience)
+    val withIssuer = JwtValidators.createDefaultWithIssuer(issuer)
+    val withAudience = DelegatingOAuth2TokenValidator(withIssuer, audienceValidator, JwtTimestampValidator())
 
-    return http.build()
+    jwtDecoder.setJwtValidator(withAudience)
+
+    return jwtDecoder
   }
 
 }
