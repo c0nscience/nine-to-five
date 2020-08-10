a2ce6f850e05f9a5974076689499f6da689f4d7f
Benjamin Herzig <ber.bherzig@pm.me>
2020-08-10 21:24:36 +0200

    remove unused entity

--- backend/src/main/kotlin/io/ntf/api/statistics/model/UserConfigurationRepository.kt
+++
@@ -1,8 +0,0 @@
-package io.ntf.api.statistics.model
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository
-import reactor.core.publisher.Mono
-
-interface UserConfigurationRepository : ReactiveMongoRepository<UserConfiguration, String> {
-    fun findByUserId(userId: String): Mono<UserConfiguration>
-}
