8d13890f6fefa66812cca90affdb92b0f9fd0165
Benjamin Herzig <ber.bherzig@pm.me>
2020-08-10 21:24:36 +0200

    remove unused entity

--- backend/src/main/kotlin/io/ntf/api/statistics/model/UserConfiguration.kt
+++
@@ -1,9 +0,0 @@
-package io.ntf.api.statistics.model
-
-import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.mapping.Document
-
-@Document(collection = "userConfigurations")
-data class UserConfiguration(@Id val id: String? = null,
-                             val userId: String,
-                             val workTimeConfiguration: WorkTimeConfiguration)
