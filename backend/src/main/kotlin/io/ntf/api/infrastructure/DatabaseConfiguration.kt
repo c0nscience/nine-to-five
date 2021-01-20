08fd594fc66f47fc858b4c4c1393b2eb07e554e8
Benjamin Herzig <ber.bherzig@pm.me>
2021-01-20 21:18:45 +0100

    revert mongodb config

--- backend/src/main/kotlin/io/ntf/api/infrastructure/DatabaseConfiguration.kt
+++ backend/src/main/kotlin/io/ntf/api/infrastructure/DatabaseConfiguration.kt
@@ -1,20 +1,8 @@
 package io.ntf.api.infrastructure
 
-import com.mongodb.Block
-import com.mongodb.MongoClientSettings
-import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.EnableMongoAuditing
-import java.util.concurrent.TimeUnit
 
 @Configuration
 @EnableMongoAuditing
-class DatabaseConfiguration : MongoClientSettingsBuilderCustomizer {
-
-  override fun customize(clientSettingsBuilder: MongoClientSettings.Builder) {
-    clientSettingsBuilder.applyToServerSettings { t ->
-      t.minHeartbeatFrequency(5000, TimeUnit.MILLISECONDS)
-        .heartbeatFrequency(30000, TimeUnit.MILLISECONDS)
-    }
-  }
-}
+class DatabaseConfiguration
