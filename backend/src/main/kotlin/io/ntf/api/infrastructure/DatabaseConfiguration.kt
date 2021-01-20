9da7f269772d4c54ee0c897ba8eee3b1f11cacf6
Benjamin Herzig <ber.bherzig@pm.me>
2021-01-20 17:39:33 +0100

    increase lower heartbeat frequency

--- backend/src/main/kotlin/io/ntf/api/infrastructure/DatabaseConfiguration.kt
+++ backend/src/main/kotlin/io/ntf/api/infrastructure/DatabaseConfiguration.kt
@@ -1,8 +1,20 @@
 package io.ntf.api.infrastructure
 
+import com.mongodb.Block
+import com.mongodb.MongoClientSettings
+import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.EnableMongoAuditing
+import java.util.concurrent.TimeUnit
 
 @Configuration
 @EnableMongoAuditing
-class DatabaseConfiguration
+class DatabaseConfiguration : MongoClientSettingsBuilderCustomizer {
+
+  override fun customize(clientSettingsBuilder: MongoClientSettings.Builder) {
+    clientSettingsBuilder.applyToServerSettings { t ->
+      t.minHeartbeatFrequency(5000, TimeUnit.MILLISECONDS)
+        .heartbeatFrequency(30000, TimeUnit.MILLISECONDS)
+    }
+  }
+}
