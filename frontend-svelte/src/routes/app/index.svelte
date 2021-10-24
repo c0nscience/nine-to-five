2de0f826a5ff29c2d84eefe24a122fcc792e1569
Benjamin Herzig <ber.bherzig@pm.me>
2021-10-24 19:53:05 +0200

    play with color scheme

--- frontend-svelte/src/routes/app/index.svelte
+++ frontend-svelte/src/routes/app/index.svelte
@@ -1,19 +1,18 @@
 <script>
-    import {isAuthenticated} from "$lib/stores/auth";
-    import auth from "$lib/services/auth";
-    import {onMount} from "svelte";
+  import auth from '$lib/services/auth'
+  import {onMount} from 'svelte'
 
-    let auth0Client;
-    onMount(async () => {
-        auth0Client = await auth.createClient();
-        const _isAuthenticated = await auth0Client.isAuthenticated();
-        console.log('onmount _isAuthenticated', _isAuthenticated)
-        if (!_isAuthenticated) {
-            await auth.login(auth0Client, {appState: {targetUrl: "/app"}});
-        }
-    });
+  let auth0Client
+  onMount(async () => {
+    auth0Client = await auth.createClient()
+    const _isAuthenticated = await auth0Client.isAuthenticated()
+    console.log('onmount _isAuthenticated', _isAuthenticated)
+    if (!_isAuthenticated) {
+      await auth.login(auth0Client, {appState: {targetUrl: '/app'}})
+    }
+  })
 </script>
 
 <div class="container mx-auto pt-12">
-    <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 text-center">App starts here</h1>
+    <h1 class="text-4xl tracking-tight font-extrabold text-gray-300 text-center">App starts here</h1>
 </div>
