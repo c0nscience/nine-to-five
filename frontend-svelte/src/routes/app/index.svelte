054540f69ce91b1a6ba6e40518666a402509cf67
Benjamin Herzig <ber.bherzig@pm.me>
2021-10-24 15:19:47 +0200

    add svelte, add auth0

---
+++ frontend-svelte/src/routes/app/index.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {isAuthenticated} from "$lib/stores/auth";
+    import auth from "$lib/services/auth";
+    import {onMount} from "svelte";
+
+    let auth0Client;
+    onMount(async () => {
+        auth0Client = await auth.createClient();
+        const _isAuthenticated = await auth0Client.isAuthenticated();
+        console.log('onmount _isAuthenticated', _isAuthenticated)
+        if (!_isAuthenticated) {
+            await auth.login(auth0Client, {appState: {targetUrl: "/app"}});
+        }
+    });
+</script>
+
+<div class="container mx-auto pt-12">
+    <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 text-center">App starts here</h1>
+</div>
