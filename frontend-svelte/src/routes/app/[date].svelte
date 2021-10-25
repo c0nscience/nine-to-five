44e38e2b6232538409d224b0e98dd6fab7ac2403
Benjamin Herzig <ber.bherzig@pm.me>
2021-10-25 17:04:23 +0200

    add simple day switcher

---
+++ frontend-svelte/src/routes/app/[date].svelte
@@ -0,0 +1,42 @@
+<script context="module">
+  import dayjs from "dayjs";
+
+  /**
+   * @type {import('@sveltejs/kit').Load}
+   */
+  export async function load({page}) {
+    if (!dayjs(page.params.date).isValid()){
+      return {
+        redirect: '/app',
+        status: 302,
+      }
+    }
+
+    return {
+      props: {
+        date: page.params.date,
+      },
+    }
+  }
+</script>
+
+<script>
+  import auth from '$lib/services/auth'
+  import {onMount} from 'svelte'
+  import DaySwitcher from "$lib/list/DaySwitcher.svelte";
+
+  export let date
+  onMount(async () => {
+    const auth0Client = await auth.createClient()
+    const _isAuthenticated = await auth0Client.isAuthenticated()
+    if (!_isAuthenticated) {
+      await auth.login(auth0Client, {appState: {targetUrl: '/app'}})
+    }
+  })
+</script>
+
+<DaySwitcher {date}/>
+
+<div class="container mx-auto">
+  <h1 class="text-4xl tracking-tight font-extrabold text-gray-200 text-center">App starts here</h1>
+</div>
