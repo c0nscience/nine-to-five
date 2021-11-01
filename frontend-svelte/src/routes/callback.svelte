0f89af58d45faaca176665e4eca5188adfde640b
Benjamin Herzig <ber.bherzig@pm.me>
2021-11-01 09:54:11 +0100

    login bug hack and vercel deployment wip

--- frontend-svelte/src/routes/callback.svelte
+++ frontend-svelte/src/routes/callback.svelte
@@ -1,33 +1,28 @@
 <script>
-  import {onMount} from 'svelte';
-  import {isAuthenticated} from '$lib/stores/auth';
-  import {goto} from "$app/navigation";
-  import {page} from "$app/stores";
+  import {error} from '$lib/stores/auth'
+  import {onMount} from 'svelte'
+  import {auth0Client} from '$lib/services/auth'
+  import {goto} from '$app/navigation'
 
-  onMount(() => {
-    console.log('routes/callback.svelte', 'onMount', 'path', $page.path)
-    setTimeout(async () => {
-      console.log('routes/callback.svelte', 'onMount', 'path', $page.path, 'setTimeout')
-      // const params = new URLSearchParams(window.location.search);
-      // if (params.has('error')) {
-      //   error.set(new Error(params.get('error_description')));
-      // }
-      //
-      // if (params.has('code')) {
-      //   const {appState} = await auth0Client.handleRedirectCallback();
-      //   console.log('routes/callback.svelte', 'onMount', 'setTimeout', 'appState', appState)
-      //   error.set(null);
-      // }
+  onMount(async () => {
+    const params = new URLSearchParams(window.location.search)
+    if (params.has('error')) {
+      error.set(new Error(params.get('error_description')))
+    }
 
-      console.log('routes/callback.svelte', 'onMount', 'path', $page.path, 'setTimeout', '$isAuthenticated', $isAuthenticated)
-      // ah that make sense we are here already in the SPA ... so we need to fill the store here for it to properly work
-      if ($isAuthenticated) {
-        await goto('/app');
-      } else {
-        await goto('/');
-      }
-    }, 0)
-  });
+    if (params.has('code')) {
+      await auth0Client.handleRedirectCallback()
+      error.set(null)
+    }
+
+    const _isAuthenticated = await auth0Client.isAuthenticated()
+    if (_isAuthenticated) {
+      await goto('/app')
+    } else {
+      await goto('/')
+    }
+  })
+
 </script>
 
-<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 text-center">Logging in</h1>
+<h1 class="text-4xl tracking-tight font-extrabold text-gray-200 text-center">Logging in</h1>
--- frontend-svelte/svelte.config.js
+++ frontend-svelte/svelte.config.js
@@ -1,8 +1,13 @@
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		target: '#svelte',
 		ssr: false,
+    adapter: adapter({
+      fallback: 'index.html'
+    }),
 	}
 };
 
