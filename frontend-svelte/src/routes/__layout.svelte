156a15df804cb8f94ecf3cba704e8ec5606f4452
Benjamin Herzig <ber.bherzig@pm.me>
2021-11-01 09:54:11 +0100

    login bug hack and vercel deployment wip

--- frontend-svelte/src/routes/__layout.svelte
+++ frontend-svelte/src/routes/__layout.svelte
@@ -1,44 +1,21 @@
 <script>
   import '../tailwind.css'
   import '../app.css'
-  import {onDestroy, onMount} from "svelte";
-  import auth, {auth0Client} from "$lib/services/auth";
-  import {error, idToken, isAuthenticated, refreshToken, user} from "$lib/stores/auth";
-  import {page} from "$app/stores";
+  import {onDestroy, onMount} from 'svelte'
+  import auth, {auth0Client} from '$lib/services/auth'
+  import {error, idToken, isAuthenticated, refreshToken, user} from '$lib/stores/auth'
 
-  let isClientCreated = false;
+  let isClientCreated = false
   const minutes = 60 * 60 * 1000
   const refreshRate = 10 * minutes
   let tokenRefreshIntervalId
 
   onMount(async () => {
-    console.log('routes/__layout.svelte', 'onMount', 'path', $page.path)
-    // setTimeout(async () => {
     await auth.createClient()
     isClientCreated = true
-    console.log('routes/__layout.svelte', 'onMount', 'path', $page.path, 'setTimeout', 'client created')
-
-    const params = new URLSearchParams(window.location.search);
-    if (params.has('error')) {
-      console.log('routes/__layout.svelte', 'onMount', 'path', $page.path, 'setTimeout', 'got error')
-      error.set(new Error(params.get('error_description')));
-    }
-
-    if (params.has('code')) {
-      console.log('routes/__layout.svelte', 'onMount', 'path', $page.path, 'setTimeout', 'got code')
-      try {
-        const {appState} = await auth0Client.handleRedirectCallback();
-        console.log('routes/__layout.svelte', 'onMount', 'path', $page.path, 'setTimeout', 'appState', appState)
-      } catch (e) {
-        console.log('routes/__layout.svelte', 'onMount', 'path', $page.path, 'setTimeout', 'handleRedirectCallback failed', e)
-      }
-      error.set(null);
-    }
 
     const _isAuthenticated = await auth0Client.isAuthenticated()
     isAuthenticated.set(_isAuthenticated)
-    console.log('routes/__layout.svelte', 'onMount', 'path', $page.path, 'setTimeout', 'isAuthenticated', _isAuthenticated)
-
     if (_isAuthenticated) {
       const _user = await auth0Client.getUser()
       user.set(_user)
@@ -47,8 +24,7 @@
       await refreshToken(auth0Client)
       tokenRefreshIntervalId = setInterval(refreshToken, refreshRate)
     }
-    // })
-  });
+  })
 
   onDestroy(() => {
     clearInterval(tokenRefreshIntervalId)
--- frontend-svelte/src/routes/app/__layout.svelte
+++ frontend-svelte/src/routes/app/__layout.svelte
@@ -1,19 +1,15 @@
 <script>
   import auth, {auth0Client} from '$lib/services/auth'
   import {onMount} from 'svelte'
-  import {page} from "$app/stores";
-  import {isAuthenticated} from "$lib/stores/auth";
+  import {page} from '$app/stores'
+  import {isAuthenticated} from '$lib/stores/auth'
 
-  onMount(() => {
-    //todo after the login from the landing page we don't register that the user is authenticated even though he is
-    console.log('routes/app/__layout.svelte', 'onMount', 'path', $page.path, $isAuthenticated)
-    // note to shelf: using the timeout to put the check later in the event loop
-    setTimeout(async () => {
-      console.log('routes/app/__layout.svelte', 'onMount', 'path', $page.path, 'setTimeout', $isAuthenticated)
-      if (!$isAuthenticated) {
-        await auth.login(auth0Client, {appState: {targetUrl: $page.params.path}})
-      }
-    }, 1)
+  onMount(async () => {
+    const _isAuthenticated = await auth0Client.isAuthenticated()
+    isAuthenticated.set(_isAuthenticated)
+    if (!_isAuthenticated) {
+      await auth.login(auth0Client, {appState: {targetUrl: $page.params.path}})
+    }
   })
 </script>
 
