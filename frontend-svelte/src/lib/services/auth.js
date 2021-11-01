7ec1adb38d18c9a25284577f4508d9193dae991f
Benjamin Herzig <ber.bherzig@pm.me>
2021-11-01 20:43:09 +0100

    fix logout redirect

--- frontend-svelte/src/lib/services/auth.js
+++ frontend-svelte/src/lib/services/auth.js
@@ -1,32 +1,32 @@
-import createAuth0Client from '@auth0/auth0-spa-js';
-import config from '$lib/config/auth_config';
+import createAuth0Client from '@auth0/auth0-spa-js'
+import config from '$lib/config/auth_config'
 
 export let auth0Client
 
 async function createClient() {
-    auth0Client = await createAuth0Client({
-        domain: config.domain,
-        client_id: config.clientId,
-        redirect_uri: `${import.meta.env.VITE_APP_URL}/callback`,
-        audience: 'https://api.ntf.io',
-        scope: 'openid read:activities start:activity stop:activity update:activity delete:activity read:metrics create:metrics delete:metrics update:metric',
-        cacheLocation: 'localstorage',
-        useRefreshTokens: true,
-    });
+  auth0Client = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId,
+    redirect_uri: `${import.meta.env.VITE_APP_URL}/callback`,
+    audience: 'https://api.ntf.io',
+    scope: 'openid read:activities start:activity stop:activity update:activity delete:activity read:metrics create:metrics delete:metrics update:metric',
+    cacheLocation: 'localstorage',
+    useRefreshTokens: true
+  })
 }
 
 async function login(client, options) {
-    await client.loginWithRedirect(options);
+  await client.loginWithRedirect(options)
 }
 
 function logout(client) {
-    return client.logout();
+  return client.logout({returnTo: import.meta.env.VITE_APP_URL})
 }
 
 const auth = {
-    createClient,
-    login,
-    logout,
-};
+  createClient,
+  login,
+  logout
+}
 
-export default auth;
+export default auth
