ec295eee5802e37f493b8fc782988d3ada1774d7
Benjamin Herzig <ber.bherzig@pm.me>
2023-01-07 11:34:30 +0100

    delete crawler

--- gpi/cmd/gpi/main.go
+++ gpi/cmd/gpi/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/c0nscience/nine-to-five/gpi/internal/activity"
 	"github.com/c0nscience/nine-to-five/gpi/internal/clock"
-	"github.com/c0nscience/nine-to-five/gpi/internal/crawler"
 	"github.com/c0nscience/nine-to-five/gpi/internal/jwt"
 	"github.com/c0nscience/nine-to-five/gpi/internal/logger"
 	"github.com/c0nscience/nine-to-five/gpi/internal/metric"
 	"github.com/c0nscience/nine-to-five/gpi/internal/store"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/gorilla/mux"
 
 	_ "github.com/heroku/x/hmetrics/onload"
@@ -134,37 +131,6 @@ func main() {
 			}
 		}
 	}()
-
-	crawlerUrl := os.Getenv("CRAWLER_URL")
-	size := os.Getenv("SIZE")
-	tgChannel := os.Getenv("TELEGRAM_CHANNEL")
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
-	if err == nil {
-		msgCooldown := time.Minute * 15
-		lastSend := time.UnixMilli(0)
-		log.Info().Msg("Got telegram credentials. Start crawler ...")
-
-		go func() {
-			for range time.Tick(time.Minute * 1) {
-				log.Info().Msg("Is in stock ...")
-
-				cli := crawler.New(crawlerUrl)
-				inStock := cli.InStock(size)
-
-				if inStock && lastSend.Add(msgCooldown).Before(clock.Now()) {
-					log.Info().Msg("Yes it was in stock.")
-					msg := tgbotapi.NewMessageToChannel(tgChannel, fmt.Sprintf("Yay! %s is in stock. GO GO GO - %s", size, crawlerUrl))
-					_, err := bot.Send(msg)
-					if err != nil {
-						log.Error().Err(err)
-					}
-					lastSend = clock.Now()
-				} else {
-					log.Info().Msg("No it was not in stock.")
-				}
-			}
-		}()
-	}
 
 	<-termChan
 
