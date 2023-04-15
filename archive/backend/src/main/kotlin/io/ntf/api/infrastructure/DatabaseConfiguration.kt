53d31c13030af6caf64bd1663f9803e81cb10ad5
Benjamin Herzig <ber.bherzig@pm.me>
2023-04-15 11:41:29 +0200

    delete archive

--- archive/backend/src/main/kotlin/io/ntf/api/infrastructure/DatabaseConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package io.ntf.api.infrastructure
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.data.mongodb.config.EnableMongoAuditing
-
-@Configuration
-@EnableMongoAuditing
-class DatabaseConfiguration
--- archive/cli/go.mod
+++
@@ -1,5 +0,0 @@
-module ninetofive.tech/cli
-
-go 1.14
-
-require rsc.io/quote/v3 v3.1.0
