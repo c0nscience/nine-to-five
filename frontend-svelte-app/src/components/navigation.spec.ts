8a7873c480289e3d3134eb29a5b0e287c3c8bc8d
Benjamin Herzig <ber.bherzig@pm.me>
2023-04-16 02:47:59 +0200

    add tests for svelte app

---
+++ frontend-svelte-app/src/components/navigation.spec.ts
@@ -0,0 +1,41 @@
+import NavigationBar from './NavigationBar.svelte'
+import { fireEvent, render, screen } from '@testing-library/svelte'
+import { DateTime } from 'luxon'
+import { currentDate } from '../stores/navigation'
+
+describe('NavigationBar', () => {
+  afterEach(() => {
+    currentDate.set(DateTime.local())
+  })
+
+  test('should render the component', async () => {
+    render(NavigationBar)
+
+    const currentDateLabel = await screen.findByTestId('current-date-label')
+    expect(currentDateLabel.innerHTML).to.equal('Today')
+  })
+
+  test('should go to previous day on left arrow click', async () => {
+    render(NavigationBar)
+
+    const btn = screen.getByTestId('prev-btn')
+    await fireEvent.click(btn)
+
+    const currentDateLabel = await screen.findByTestId('current-date-label')
+
+    const expected = DateTime.local().minus({ day: 1 }).toFormat('EEE, DD')
+    expect(currentDateLabel.innerHTML).to.equal(expected)
+  })
+
+  test('should go to next day on right arrow click', async () => {
+    render(NavigationBar)
+
+    const btn = screen.getByTestId('next-btn')
+    await fireEvent.click(btn)
+
+    const currentDateLabel = await screen.findByTestId('current-date-label')
+
+    const expected = DateTime.local().plus({ day: 1 }).toFormat('EEE, DD')
+    expect(currentDateLabel.innerHTML).to.equal(expected)
+  })
+})
