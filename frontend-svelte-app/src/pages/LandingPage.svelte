efb16560394de4bd683a7a864e37232ae01ac9c8
Benjamin Herzig <ber.bherzig@pm.me>
2023-04-16 20:03:18 +0200

    refine landing page

--- frontend-svelte-app/src/pages/LandingPage.svelte
+++ frontend-svelte-app/src/pages/LandingPage.svelte
@@ -1,13 +1,13 @@
 <div class="hero min-h-screen">
   <div class="hero-content text-center">
-    <div class="max-w-md">
+    <article class="max-w-md prose">
       <h1 class="text-5xl font-bold">Keep track</h1>
-      <p class="py-6">With 9to5, you can easily track your tasks throughout the day. Tags help you categorize them
-        quickly, allowing you to analyze how much time you spend on various types of tasks.</p>
+      <p>With <strong>9to5</strong>, you can easily track your tasks throughout the day. Tags help you
+        categorize them quickly, allowing you to analyze how much time you spend on various types of tasks.</p>
       <a class="btn btn-primary"
          href="/activities">
         Let's go!
       </a>
-    </div>
+    </article>
   </div>
 </div>
