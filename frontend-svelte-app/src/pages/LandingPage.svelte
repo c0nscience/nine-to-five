eddcdb6020c3cb16a640e687d8d7719b11fb1f03
Benjamin Herzig <ber.bherzig@pm.me>
2023-04-16 19:48:32 +0200

    first draft of landing page text

--- frontend-svelte-app/src/pages/LandingPage.svelte
+++ frontend-svelte-app/src/pages/LandingPage.svelte
@@ -1,9 +1,9 @@
 <div class="hero min-h-screen">
   <div class="hero-content text-center">
     <div class="max-w-md">
-      <h1 class="text-5xl font-bold">Hello there</h1>
-      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi.
-        In deleniti eaque aut repudiandae et a id nisi.</p>
+      <h1 class="text-5xl font-bold">Keep track</h1>
+      <p class="py-6">With 9to5, you can easily track your tasks throughout the day. Tags help you categorize them
+        quickly, allowing you to analyze how much time you spend on various types of tasks.</p>
       <a class="btn btn-primary"
          href="/activities">
         Let's go!
