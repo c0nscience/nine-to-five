87ab444c0d1ce9b08c1bd8a06c683af9a2cf0f40
Benjamin Herzig <ber.bherzig@pm.me>
2023-11-29 19:01:29 +0100

    delete sveltekit app, a bit tinkering on the svelte app

--- frontend-svelte/src/hook.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ event, resolve }) {
-  const response = await resolve(event, {
-    ssr: false,
-    transformPage: ({ html }) => html.replace('old', 'new')
-  });
-
-  return response;
-}
