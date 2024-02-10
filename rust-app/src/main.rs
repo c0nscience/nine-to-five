1f5cc2f4a203188f3a1b8bee0fbbdb4e4e9c1039
Benjamin Herzig <ber.bherzig@pm.me>
2024-02-11 00:51:31 +0100

    basic axum, askama setup

--- rust-app/src/main.rs
+++ rust-app/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use askama::Template;
+use axum::{response::IntoResponse, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(index));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
+
+async fn index() -> impl IntoResponse {
+    IndexTemplate {}
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {}
