78ef7f8604a5057ba6f8f590f799c4dfe989baf7
Benjamin Herzig <ber.bherzig@pm.me>
2024-02-13 01:42:02 +0100

    feat(rust-app): finish authentication round trip

--- rust-app/src/main.rs
+++ rust-app/src/main.rs
@@ -1,25 +1,40 @@
-use std::{collections::HashMap, time::Duration};
-
 use askama::Template;
 use axum::{
-    extract::{Query, State},
+    extract::{FromRef, Query, Request, State},
+    http::StatusCode,
+    middleware::{self, Next},
     response::{IntoResponse, Redirect},
     routing::get,
     Router,
 };
+use axum_extra::extract::{cookie::Cookie, cookie::Key, PrivateCookieJar};
+use chrono::{Duration, Local};
 use oauth2::{
     basic::BasicClient, reqwest::async_http_client, AuthUrl, AuthorizationCode, ClientId,
-    ClientSecret, CsrfToken, PkceCodeChallenge, PkceCodeVerifier, RedirectUrl, TokenUrl,
+    ClientSecret, CsrfToken, PkceCodeChallenge, PkceCodeVerifier, RedirectUrl, TokenResponse,
+    TokenUrl,
 };
 use serde::Deserialize;
-use sqlx::{postgres::PgPoolOptions, PgPool};
+use sqlx::{
+    postgres::PgPoolOptions, // PgPool,
+};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use tracing::error;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Clone)]
 struct AppState {
-    db: PgPool,
+    // db: PgPool,
     oauth_client: BasicClient,
-    verifiers: HashMap<String, PkceCodeVerifier>,
+    key: Key,
+    verifiers: Arc<Mutex<HashMap<String, String>>>,
+}
+
+impl FromRef<AppState> for Key {
+    fn from_ref(input: &AppState) -> Self {
+        input.key.clone()
+    }
 }
 
 #[tokio::main]
@@ -35,9 +50,9 @@ async fn main() {
     let db_connection_str = std::env::var("DATABASE_URL")
         .unwrap_or_else(|_| "postgres://postgres:rust@localhost".to_string());
 
-    let pool = PgPoolOptions::new()
+    let _db = PgPoolOptions::new()
         .max_connections(5)
-        .acquire_timeout(Duration::from_secs(3))
+        .acquire_timeout(std::time::Duration::from_secs(3))
         .connect(&db_connection_str)
         .await
         .expect("can not connect to database");
@@ -46,14 +61,25 @@ async fn main() {
     let client_secret = std::env::var("CLIENT_SECRET").unwrap();
     let oauth_client = build_oauth_client(client_id, client_secret);
 
+    let verifiers = HashMap::new();
     let state = AppState {
-        db: pool,
+        // db,
         oauth_client,
-        verifiers: HashMap::new(),
+        key: Key::generate(),
+        verifiers: Arc::new(Mutex::new(verifiers)),
     };
+
+    let protected_route =
+        Router::new()
+            .route("/", get(protected))
+            .route_layer(middleware::from_fn_with_state(
+                state.clone(),
+                check_authorized,
+            ));
 
     let app = Router::new()
         .route("/", get(index))
+        .nest("/app", protected_route)
         .route("/login", get(login))
         .route("/callback", get(callback))
         .with_state(state);
@@ -66,19 +92,24 @@ async fn index() -> impl IntoResponse {
     IndexTemplate {}
 }
 
+async fn protected() -> impl IntoResponse {
+    ProtectedTemplate {}
+}
+
 async fn login(State(state): State<AppState>) -> Redirect {
     let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
-    dbg!(pkce_challenge.as_str());
-    dbg!(pkce_verifier);
-    let (auth_url, _csrf_token) = state
+
+    let (auth_url, csrf_token) = state
         .oauth_client
         .authorize_url(CsrfToken::new_random)
         .set_pkce_challenge(pkce_challenge)
         .url();
-    dbg!(_csrf_token.secret());
-    state
-        .verifiers
-        .insert(_csrf_token.secret().to_string(), pkce_verifier);
+
+    state.verifiers.lock().unwrap().insert(
+        csrf_token.secret().to_string(),
+        pkce_verifier.secret().to_string(),
+    );
+
     Redirect::temporary(auth_url.as_str())
 }
 
@@ -105,22 +136,64 @@ struct AuthRequest {
     state: String,
 }
 
-async fn callback(State(state): State<AppState>, auth_request: Query<AuthRequest>) -> Redirect {
-    let auth_request = auth_request.0;
+async fn callback(
+    State(state): State<AppState>,
+    jar: PrivateCookieJar,
+    Query(auth_request): Query<AuthRequest>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    let pkce_verifier = {
+        let verifiers = state.verifiers.lock().unwrap();
+        verifiers.get(&auth_request.state).unwrap().into()
+    };
+    let pkce_verifier = PkceCodeVerifier::new(pkce_verifier);
 
-    dbg!(auth_request.state);
-
-    let pkce_verifier = state.verifiers.get(auth_request.state);
-
-    let _token_result = state
+    let token = match state
         .oauth_client
         .exchange_code(AuthorizationCode::new(auth_request.code))
         .set_pkce_verifier(pkce_verifier)
-        .request_async(async_http_client);
+        .request_async(async_http_client)
+        .await
+    {
+        Ok(res) => res,
+        Err(e) => {
+            error!("could not exchange code: {e}");
+            return Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string()));
+        }
+    };
 
-    Redirect::temporary("/")
+    let secs: i64 = token.expires_in().unwrap().as_secs().try_into().unwrap();
+
+    let _max_age = Local::now().naive_local() + Duration::seconds(secs);
+
+    let cookie = Cookie::build(("sid", token.access_token().secret().to_owned()))
+        .domain(".localhost")
+        .path("/")
+        .secure(true)
+        .http_only(true)
+        .max_age(time::Duration::seconds(secs));
+
+    Ok((jar.add(cookie), Redirect::temporary("/app")))
+}
+
+async fn check_authorized(
+    State(_state): State<AppState>,
+    jar: PrivateCookieJar,
+    mut req: Request,
+    next: Next,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    let Some(cookie) = jar.get("sid").map(|cookie| cookie.value().to_owned()) else {
+        return Err((StatusCode::UNAUTHORIZED, "Unauthorized!".to_string()));
+    };
+
+    req.extensions_mut().insert(cookie);
+
+    Ok(next.run(req).await)
 }
 
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexTemplate {}
+
+#[derive(Template)]
+#[template(path = "app.html")]
+struct ProtectedTemplate {}
