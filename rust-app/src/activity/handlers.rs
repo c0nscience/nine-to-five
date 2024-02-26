00b53bd79e8b34179d17f8465bc1bba551ba7a29
Benjamin Herzig <ber.bherzig@pm.me>
2024-02-26 17:33:45 +0100

    work on activity list ui and navigation

--- rust-app/src/activity/handlers.rs
+++ rust-app/src/activity/handlers.rs
@@ -28,6 +28,22 @@ pub fn router(state: crate::states::AppState) -> Router<AppState> {
         ))
 }
 
+struct ActivityTemplate {
+    id: sqlx::types::Uuid,
+    user_id: String,
+    name: String,
+    start_time: chrono::DateTime<chrono::Utc>,
+    end_time: Option<chrono::DateTime<chrono::Utc>>,
+    duration: chrono::TimeDelta,
+    tags: Vec<TagTemplate>,
+}
+
+pub struct TagTemplate {
+    id: sqlx::types::Uuid,
+    user_id: String,
+    name: String,
+}
+
 async fn activities(
     Path(date): Path<String>,
     State(state): State<crate::states::AppState>,
@@ -37,8 +53,44 @@ async fn activities(
     let Some(end) = start.succ_opt() else {
         return Err(crate::errors::AppError::InternalError);
     };
+    let Some(prev) = start.pred_opt() else {
+        return Err(crate::errors::AppError::InternalError);
+    };
+    let date = if Utc::now().date_naive() == start {
+        "Today".to_string()
+    } else {
+        date
+    };
     let activities = crate::activity::in_range(state.db, user_id, start, end).await?;
-    Ok(ActivitiesTemplate { activities, date })
+    let activities = activities
+        .iter()
+        .map(|a| {
+            let duration = a.end_time.unwrap_or_else(Utc::now) - a.start_time;
+            ActivityTemplate {
+                id: a.id,
+                user_id: a.user_id.clone(),
+                name: a.name.clone(),
+                start_time: a.start_time,
+                end_time: a.end_time,
+                duration,
+                tags: a
+                    .tags
+                    .iter()
+                    .map(|t| TagTemplate {
+                        id: t.id,
+                        user_id: t.user_id.clone(),
+                        name: t.name.clone(),
+                    })
+                    .collect(),
+            }
+        })
+        .collect();
+    Ok(ActivitiesTemplate {
+        activities,
+        date,
+        prev,
+        next: end,
+    })
 }
 
 async fn new_activity(
@@ -50,8 +102,10 @@ async fn new_activity(
 #[derive(Template)]
 #[template(path = "activities.html")]
 struct ActivitiesTemplate {
-    activities: Vec<crate::activity::Activity>,
+    activities: Vec<ActivityTemplate>,
     date: String,
+    prev: chrono::NaiveDate,
+    next: chrono::NaiveDate,
 }
 #[derive(Template)]
 #[template(path = "activities/create_modal.html")]
