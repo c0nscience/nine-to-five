2806914530363d7a4053666e67f86dc5e5de8925
Benjamin Herzig <ber.bherzig@pm.me>
2024-02-26 22:44:53 +0100

    add create activity form and store in database

--- rust-app/src/activity/handlers.rs
+++ rust-app/src/activity/handlers.rs
@@ -1,19 +1,24 @@
 use askama::Template;
 use axum::{
     extract::{Path, State},
+    http::StatusCode,
     middleware,
     response::{IntoResponse, Redirect},
-    routing::get,
-    Extension, Router,
+    routing::{get, post},
+    Extension, Form, Router,
 };
 use chrono::prelude::*;
+use serde::Deserialize;
 
 use crate::states::AppState;
+
+use super::StoreActivity;
 
 pub fn router(state: crate::states::AppState) -> Router<AppState> {
     Router::new()
         .route("/:date", get(activities))
         .route("/new", get(new_activity))
+        .route("/new", post(create_new_activity))
         .route(
             "/",
             get(|| async {
@@ -28,20 +33,30 @@ pub fn router(state: crate::states::AppState) -> Router<AppState> {
         ))
 }
 
-struct ActivityTemplate {
-    id: sqlx::types::Uuid,
-    user_id: String,
+struct ActivityTemplData {
+    // id: sqlx::types::Uuid,
+    // user_id: String,
     name: String,
-    start_time: chrono::DateTime<chrono::Utc>,
-    end_time: Option<chrono::DateTime<chrono::Utc>>,
-    duration: chrono::TimeDelta,
-    tags: Vec<TagTemplate>,
+    // start_time: chrono::DateTime<chrono::Utc>,
+    // end_time: Option<chrono::DateTime<chrono::Utc>>,
+    duration: String,
+    duration_iso: String,
+    tags: Vec<TagTemplData>,
 }
 
-pub struct TagTemplate {
-    id: sqlx::types::Uuid,
-    user_id: String,
+pub struct TagTemplData {
+    // id: sqlx::types::Uuid,
+    // user_id: String,
     name: String,
+}
+
+#[derive(Template)]
+#[template(path = "activities.html")]
+struct ActivitiesTemplate {
+    activities: Vec<ActivityTemplData>,
+    date: String,
+    prev: chrono::NaiveDate,
+    next: chrono::NaiveDate,
 }
 
 async fn activities(
@@ -59,26 +74,31 @@ async fn activities(
     let date = if Utc::now().date_naive() == start {
         "Today".to_string()
     } else {
-        date
+        start.format("%a, %b %d, %Y").to_string()
     };
     let activities = crate::activity::in_range(state.db, user_id, start, end).await?;
     let activities = activities
         .iter()
         .map(|a| {
             let duration = a.end_time.unwrap_or_else(Utc::now) - a.start_time;
-            ActivityTemplate {
-                id: a.id,
-                user_id: a.user_id.clone(),
+            let duration_iso = format!("{duration}");
+            let mintues = (duration.num_seconds() / 60) % 60;
+            let hours = (duration.num_seconds() / 60) / 60;
+            let duration = format!("{hours}h {mintues}m");
+            ActivityTemplData {
+                // id: a.id,
+                // user_id: a.user_id.clone(),
                 name: a.name.clone(),
-                start_time: a.start_time,
-                end_time: a.end_time,
+                // start_time: a.start_time,
+                // end_time: a.end_time,
                 duration,
+                duration_iso,
                 tags: a
                     .tags
                     .iter()
-                    .map(|t| TagTemplate {
-                        id: t.id,
-                        user_id: t.user_id.clone(),
+                    .map(|t| TagTemplData {
+                        // id: t.id,
+                        // user_id: t.user_id.clone(),
                         name: t.name.clone(),
                     })
                     .collect(),
@@ -93,20 +113,37 @@ async fn activities(
     })
 }
 
+#[derive(Template)]
+#[template(path = "activities/create_modal.html")]
+struct NewActivityTemplate {}
+
 async fn new_activity(
     State(_state): State<crate::states::AppState>,
     Extension(_user_id): Extension<String>,
 ) -> Result<impl IntoResponse, crate::errors::AppError> {
     Ok(NewActivityTemplate {})
 }
-#[derive(Template)]
-#[template(path = "activities.html")]
-struct ActivitiesTemplate {
-    activities: Vec<ActivityTemplate>,
-    date: String,
-    prev: chrono::NaiveDate,
-    next: chrono::NaiveDate,
+
+#[derive(Debug, Deserialize)]
+struct CreateActivity {
+    name: String,
 }
-#[derive(Template)]
-#[template(path = "activities/create_modal.html")]
-struct NewActivityTemplate {}
+
+async fn create_new_activity(
+    State(state): State<crate::states::AppState>,
+    Extension(user_id): Extension<String>,
+    Form(create_activity): Form<CreateActivity>,
+) -> Result<StatusCode, crate::errors::AppError> {
+    let _ = crate::activity::create(
+        state.db,
+        StoreActivity {
+            user_id,
+            name: create_activity.name,
+            start_time: Utc::now(),
+            end_time: None,
+            tags: Vec::new(),
+        },
+    )
+    .await?;
+    Ok(StatusCode::NO_CONTENT)
+}
