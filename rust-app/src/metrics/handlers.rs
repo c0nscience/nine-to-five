62548a167c3340587deb52bbd23a3bee8dfac124
Benjamin Herzig <ber.bherzig@pm.me>
2024-03-23 14:04:25 +0100

    start metric feature

--- rust-app/src/metrics/handlers.rs
+++ rust-app/src/metrics/handlers.rs
@@ -1,26 +1,21 @@
-
 use askama::Template;
 use axum::{
-    extract::{State},
+    extract::State,
     middleware,
-    response::{IntoResponse},
-    routing::{get},
+    response::{IntoResponse, Redirect},
+    routing::get,
     Extension, Router,
 };
 
-
-
-
-
-
 use tracing::info;
 
-
-use crate::{auth, errors, states};
+use super::*;
+use crate::{activity, auth, errors, states};
 
 pub fn router(state: states::AppState) -> Router<states::AppState> {
     Router::new()
-        .route("/", get(list))
+        .route("/", get(list).post(create_metric))
+        .route("/new", get(new_form))
         .route_layer(middleware::from_fn_with_state(
             state,
             auth::check_authorized,
@@ -38,4 +33,25 @@ async fn list(
     info!("user_id: {}", user_id);
 
     Ok(MetricsTemplate {})
+}
+
+#[derive(Template)]
+#[template(path = "metrics/new_form.html")]
+struct NewMetricTemplate {
+    available_tags: Vec<activity::AvailableTag>,
+}
+
+async fn new_form(
+    State(state): State<states::AppState>,
+    Extension(user_id): Extension<String>,
+) -> Result<impl IntoResponse, errors::AppError> {
+    let available_tags = activity::available_tags(&state.db, user_id).await?;
+    Ok(NewMetricTemplate { available_tags })
+}
+
+async fn create_metric(
+    State(state): State<states::AppState>,
+    Extension(user_id): Extension<String>,
+) -> Result<impl IntoResponse, errors::AppError> {
+    Ok(Redirect::to("/app/metrics"))
 }
